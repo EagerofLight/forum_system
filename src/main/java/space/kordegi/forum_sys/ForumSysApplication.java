3022357214ed71ec46b88ae8e08bb5ff20d18d90
Wilson Lin <114272565+EagerofLight@users.noreply.github.com>
2024-03-06 01:05:22 -0500

    Initial

---
+++ src/main/java/space/kordegi/forum_sys/ForumSysApplication.java
@@ -0,0 +1,13 @@
+package space.kordegi.forum_sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumSysApplication.class, args);
+    }
+
+}
