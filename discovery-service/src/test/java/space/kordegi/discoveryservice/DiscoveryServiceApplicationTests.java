581d892ad004468fd97dc59380b503df043b07cc
Wilson Lin <114272565+EagerofLight@users.noreply.github.com>
2024-03-07 17:01:01 -0500

    config service cannot use, temp use app.yaml, add discovery service

---
+++ discovery-service/src/test/java/space/kordegi/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package space.kordegi.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
--- forum-service/forum-core/src/main/java/space/kordegi/forumcore/ForumCoreApplication.java
+++ forum-service/forum-core/src/main/java/space/kordegi/forumcore/ForumCoreApplication.java
@@ -2,8 +2,12 @@ package space.kordegi.forumcore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
+@EnableDiscoveryClient
 public class ForumCoreApplication {
 
   public static void main(String[] args) {
